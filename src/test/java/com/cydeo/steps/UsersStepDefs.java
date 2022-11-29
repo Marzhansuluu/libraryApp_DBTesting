203bac08690c62f74cd7454f90f082ad966c3c26
mehmetCt <mehmet@cybertekschool.com>
2022-11-29 12:20:03 -0500

    users.feature

--- src/test/java/com/cydeo/steps/UsersStepDefs.java
+++ src/test/java/com/cydeo/steps/UsersStepDefs.java
@@ -2,14 +2,17 @@ package com.cydeo.steps;
 
 import com.cydeo.pages.UsersPage;
 import com.cydeo.utility.BrowserUtil;
+import com.cydeo.utility.DB_Util;
 import io.cucumber.java.en.*;
 import io.cucumber.java.eo.Se;
+import org.junit.Assert;
 import org.openqa.selenium.support.ui.Select;
 
 public class UsersStepDefs {
 
     UsersPage usersPage=new UsersPage();
     String email;
+    String expectedStatus;
 
     @When("the user clicks Edit User button")
     public void the_user_clicks_edit_user_button() {
@@ -30,7 +33,7 @@ public class UsersStepDefs {
 
         BrowserUtil.waitFor(2);
 
-
+        expectedStatus=inactive;
     }
     @When("the user clicks save changes button")
     public void the_user_clicks_save_changes_button() {
@@ -44,10 +47,20 @@ public class UsersStepDefs {
         // Maybe this message will appear dynamicly.In that case  you need to handle time issue with Explicit Wait
         BrowserUtil.waitFor(1);
         String actualMessage = usersPage.toastMessage.getText();
+        Assert.assertEquals(expectedMessage,actualMessage);
 
     }
     @Then("the users should see same status for related user in database")
     public void the_users_should_see_same_status_for_related_user_in_database() {
+
+        DB_Util.runQuery("select status from users where email='"+email+"'");
+
+        //Get data
+        String actualStatus = DB_Util.getFirstRowFirstColumn();
+
+        //verify
+        Assert.assertEquals(expectedStatus,actualStatus);
+
 
     }
     @Then("the user changes current user status {string} to {string}")
